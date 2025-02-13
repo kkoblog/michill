6a221fc4c1ad0c7abac0acdef1fd89a610622b78
kkoblog <koba0721a@gmail.com>
2025-02-14 05:51:55 +0900

    コミットメッセージ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out forwards',
+      },
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          '100%': { opacity: '1' }
         }
-      },
-      animation: {
-        fadeIn: 'fadeIn 1.5s ease-out forwards',
       }
     }
   },
-  plugins: []
+  plugins: [],
 }
